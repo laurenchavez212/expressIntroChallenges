5e48bcc2c5c7894fb9e02bc159620febc8eea475
Lauren Chavez <laurenchavez@Laurens-MacBook-Pro.local>
2018-08-02 15:56:35 -0700

    added with finished challenges

--- part1/server.js
+++ part1/server.js
@@ -1,10 +1,34 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+var fs = require('fs');
 
 app.get('/yourroute', function(req, res) {
   res.send("stuff");
 });
+
+app.get('/hello', (req, res)=> {
+  res.send('Hello!')
+});
+
+app.post('/create/:name', (req, res)=> {
+  var hi = {
+    "id":1,
+    "name": req.params.name,
+  };
+  res.json(hi)
+});
+
+app.get('/', (req, res)=> {
+  var data = fs.readFileSync("part1/index.html", 'utf8');
+  res.send(data)
+})
+
+app.get('/verify/:age', (req, res)=> {
+  if(req.params.age > 13) {
+    res.send(200)
+  } else (res.send(403))
+})
 
 app.use(function(req, res) {
   res.sendStatus(404);
--- part2/server.js
+++ part2/server.js
@@ -1,10 +1,49 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+var fs = require('fs');
 
 app.get('/yourroute', function(req, res) {
   res.send("stuff");
 });
+
+app.post('/create/:name/:age', (req, res)=> {
+  var obj = {
+    name: req.params.name,
+    age: parseInt(req.params.age),
+  };
+  fs.readFile("storage.json", 'utf8', (err, data)=> {
+    let arr = JSON.parse(data);
+    arr.push(obj);
+    fs.writeFile('./storage.json', JSON.stringify(arr), (err, data)=> {
+      if(err){
+        throw err;
+      }
+      res.sendStatus(200)
+    })
+  })
+})
+
+app.get('/', (req, res)=> {
+  fs.readFile('storage.json', 'utf8', (err, data)=> {
+    res.send(data)
+  })
+})
+
+app.get('/:name', (req, res)=> {
+  fs.readFile('storage.json', 'utf8', (err, data)=>{
+    let pd = JSON.parse(data);
+    let match = pd.filter((item)=> {
+      return item.name == req.params.name;
+
+    });
+    if(match.length >= 1) {
+      res.send(match[0])
+    } else {
+      res.sendStatus(400)
+    }
+  })
+})
 
 app.use(function(req, res) {
   res.sendStatus(404);
